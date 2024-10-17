cf74815a171ab64d22e58c32c3a49ad497ddfd38
Sakshi Verma <sakshiv1011@gmail.com>
2024-10-17 11:06:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let scrollContainer=document.querySelector(".gallery");
+let backbtn=document.getElementById("backBtn");
+let nextbtn=document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel",(evt)=>{
+    evt.preventDefault();
+    scrollContainer.scrollLeft +=evt.deltaY;
+    scrollContainer.style.scrollBehavior='auto';
+})
+
+nextbtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior='smooth';
+    scrollContainer.scrollLeft +=900;
+});
+
+backbtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior='smooth';
+    scrollContainer.scrollLeft -=900;
+});
